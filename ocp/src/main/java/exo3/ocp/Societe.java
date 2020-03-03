b9d19936b8fca2e3625e0d98baa3af3126898c16
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-04 00:16:15 +0100

    la classe Sociéte qui s'en charge de calculer la sommes de tous les salaires des employés de la sociéte

---
+++ ocp/src/main/java/exo3/ocp/Societe.java
@@ -0,0 +1,44 @@
+package exo3.ocp;
+
+import java.util.ArrayList;
+
+public class Societe {
+	
+	private ArrayList<Employe> employes_societe;
+	
+	public  Societe () {
+		
+		employes_societe= new ArrayList<Employe>();
+	}
+	
+	/* Ajouter un employer a la liste */
+	
+	public void ajouter_empolye(final Employe e) {
+		
+		if (!employes_societe.contains(e)) {
+			employes_societe.add(e);
+		}
+		}
+	
+	
+	public void supprimer_employe(final Employe e) {
+		
+		if (employes_societe.contains(e)) {
+			employes_societe.remove(e);
+		}
+	}
+	
+
+	/* méthode pour calculer la somme des salaires de tous les employes  de la sociéte*/
+	
+	public int calculSommeSalaires() {
+		int somme_salaires=0;
+		
+		for(Employe employe: employes_societe) {
+			 somme_salaires+= employe.salaire();
+		}
+		
+		return somme_salaires;
+	}
+	
+}
