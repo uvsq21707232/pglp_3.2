25327ecf1d2c202807bd7de4b1a244ad21ae0437
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-04 10:46:21 +0100

    test classe Employe

---
+++ ocp/src/test/java/exo3/ocp/TestEmploye.java
@@ -0,0 +1,13 @@
+package exo3.ocp;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestEmploye {
+	
+	@Test
+	public void testSalaire() throws Exception {
+		Employe emp= new Employe("roger",2019);
+		assertTrue(emp.salaire() == 1500);// salaire=1520
+	}
+
+}
