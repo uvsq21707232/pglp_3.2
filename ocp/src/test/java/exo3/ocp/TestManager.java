8548b4bacee1eeaedf989d643222e25b60ed913e
abdelhafid belhabib <abdelhafid.belhabib@ens.uvsq.fr>
2020-03-04 10:58:00 +0100

    test classe Manager

---
+++ ocp/src/test/java/exo3/ocp/TestManager.java
@@ -0,0 +1,14 @@
+package exo3.ocp;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class TestManager {
+	
+	/* tester la méthode salaire de la classe Manager*/
+	@Test
+	public void testSalaireManager() {
+		Manager manager = new Manager("john",2019,2);
+		assertTrue(manager.salaire() == 1500 + 200);//salaire 1720
+	}
+
+}
--- ocp/src/test/java/exo3/ocp/TestVendeur.java
+++ ocp/src/test/java/exo3/ocp/TestVendeur.java
@@ -8,7 +8,7 @@ public class TestVendeur {
 
 	/* tester la méthode salire de la classe vendeur*/
 	@Test
-	public void testSalireVendeur() {
+	public void testSalaireVendeur() {
 		Vendeur vendeur = new Vendeur("nadal",2019,300);
 		assertTrue(vendeur.salaire() == 1500 + 300);//salaire 1820
 	}
